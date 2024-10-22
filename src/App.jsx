ab1637a5d409130ea13b9194a53073fd2a0184d1
JoseLuisAlvarezManica <114442646+JoseLuisAlvarezManica@users.noreply.github.com>
2024-10-21 19:01:02 -0600

    Cambios finales

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Menu } from './assets/components/Menu'
+import { Menu } from './components/Menu'
 
 function App() {
   const [count, setCount] = useState(0)
